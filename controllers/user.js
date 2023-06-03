d4e255d7556e1cf6580b072a73b7bfe6b9fd423f
Akindejoye <tydruhill@gmail.com>
2023-06-03 17:40:53 +0100

    Update User Details Route

--- controllers/user.js
+++ controllers/user.js
@@ -36,6 +36,26 @@ export const getUser = async (req, res) => {
   }
 }
 
+export const updateUserData = async (req, res) => {
+  try {
+    const { id: userId } = req.params;
+
+    const user = await User.findOneAndUpdate({ _id: userId }, req.body, {
+      new: true,
+      runValidators: true,
+    })
+
+    if(!user) {
+      return res.status(404).json({ msg: `No user with id: ${userId}` });
+    }
+
+    res.status(200).json({ user });
+
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+}
+
 export const deleteUser = async (req, res) => {
   try {
     const { id: userId } = req.params;
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { addUser, getAllUsers, getUser, deleteUser } from '../controllers/user.js';
+import { addUser, getAllUsers, getUser, updateUserData, deleteUser } from '../controllers/user.js';
 
 const router = express.Router();
 
 router.post('/', addUser);
 router.get('/', getAllUsers);
 router.get('/:id', getUser);
+router.patch('/:id', updateUserData);
 router.delete('/:id', deleteUser);
 
 export default router;
